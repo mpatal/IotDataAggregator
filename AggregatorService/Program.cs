7b6e2eee25a60137e5527ccf96405f864c1f767b
Marvin Patal <marvin.patal@hotmail.com>
2018-04-18 14:00:09 -0400

    initial commit

---
+++ AggregatorService/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AggregatorService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel(
+                     options =>
+                    {
+                        options.UseConnectionLogging();
+                    }
+                )
+                .UseUrls("http://+:5000")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
